ba8974b6f284572d4e5eac5d61cf47645953f5d8
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-01-31 20:56:22 +0530

    actions

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PERSON1=$1 # no space between value and =
-PERSON2=$2
+PERSON2=$2 # $2 represents the second argument passed to the script. These are special variables in shell scripting.
 
 echo "$PERSON1:: Hello $PERSON2, How are you?"
 echo "$PERSON2:: Hi $PERSON1, I am good. How about you?"
--- 05-variables.sh
+++ 05-variables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
  echo "Please Enter your name:"
- read name
+ read name # read is used to take input from user
 
  echo "Your name is $name"
 
  echo "Please enter your password:"
- read -s pwd
+ read -s pwd # now it will not show the password on the terminal for security purpose
