7f357ebe8b142127b8bf626ea45add38b874454f
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-01-31 20:17:43 +0530

    Updated

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/bash 
+# shebang is the first line in the shell script that represents the path to the interpreter.what ever we write inside shell script that will be executed by this shebang
 
 # This is the comment, this will not be executed by bash. This is only for information purpose.
 echo "Hello, World!"
---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Suresh:: Hello Ramesh, How are you?"
+echo "Ramesh:: Hi Suresh, I am good. How about you?"
+echo "Suresh:: I am doing great. What are you learning Ramesh?"
+echo "Ramesh:: I am learning Shell Scripting. what about you?"
