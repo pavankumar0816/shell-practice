b6c4985d7d20eb21d61c9268fef10efb5a03539b
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-01-31 20:26:03 +0530

    actions

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1="Suresh"
-PERSON2="Ramesh"
+PERSON1="Pavan"
+PERSON2="Sai"
 
 echo "$PERSON1:: Hello $PERSON2, How are you?"
 echo "$PERSON2:: Hi $PERSON1, I am good. How about you?"
