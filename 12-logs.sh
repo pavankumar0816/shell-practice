7477cf7b1be4025dc13f2855d4cff02c227929b3
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-02-02 22:11:56 +0530

    actions

---
+++ 12-logs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+userid=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $userid -ne 0 ]; then
+   echo "Please run this script with root user access"
+   exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+validate(){
+   if( $1 != 0 ); then
+        echo "$2 is failure"
+        exit 1
+    else
+        echo "$2 is success"
+    fi 
+}
+
+dnf install nginx -y &>> $LOGS_FILE
+validate $? "Nginx Installation"
+
+dnf install mysql -y &>> $LOGS_FILE
+validate $? "Mysql Installation"
+
+dnf install nodejs -y &>> $LOGS_FILE
+validate $? "Nodejs Installation"
