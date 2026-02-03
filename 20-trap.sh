5222bacbd04160d500a09a415b7fc98d76f82515
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-02-03 10:40:15 +0530

    actions

---
+++ 20-trap.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+userid=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $userid -ne 0 ]; then
+   echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+   exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+for package in $@ # sudo sh 14-loops.sh nginx mysql nodejs
+do
+    dnf list installed $package &>>$LOGS_FILE
+    if [ $? -ne 0 ]; then
+        echo "$package is not installed, Installing now" 
+        dnf install $package -y &>>$LOGS_FILE
+    else
+        echo -e "$package is already installed, $Y Skipping installation $N"
+    fi
+done
