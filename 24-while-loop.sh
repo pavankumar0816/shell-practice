d560785339d297a46f0ff5f6337cec8d87223cda
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-02-07 22:29:33 +0530

    Created delete log file

--- 24-while-loop.sh
+++ 24-while-loop.sh
@@ -2,8 +2,13 @@
 
 count=1
 
-while [ $count -le 5 ]
-do 
-echo "Count is $count"
-((count++))
-done
+# while [ $count -le 5 ]
+# do 
+# echo "Count is $count"
+# ((count++))
+# done
+
+while IFS= read -r line; do
+# Process each line here
+echo "$line"
+done < ./21-script-1.sh # input which file to read
