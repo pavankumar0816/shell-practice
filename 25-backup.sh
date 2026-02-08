79c265c96b32f1f838daf309b180b530cf716a5d
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-02-08 16:37:07 +0530

    Updated

--- 25-backup.sh
+++ 25-backup.sh
@@ -7,6 +7,9 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14} # 14 days is the default value, if the user not supplied
 
 if [ $userid -ne 0 ]; then
    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
@@ -23,3 +26,15 @@ USAGE(){
 if [ $# -le 2 ]; then
    USAGE
 fi
+
+if [ ! -d $SOURCE_DIR ]; then
+   echo -e "$Y Source Directory Doest not exist $N"
+   exit 1
+fi
+
+if [ ! -d $DEST_DIR ]; then
+   echo -e "$Y Destination Directory Does not exist $N"
+   exit 1
+fi
+
+
