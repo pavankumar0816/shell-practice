143b2ff9079d98f25d4fb73b4c8652c7136a54d2
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-02-08 16:30:44 +0530

    Updated

---
+++ 25-backup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+userid=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/backup.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $userid -ne 0 ]; then
+   echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+   exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+USAGE(){
+    echo -e "$R USAGE:: sudo backup <source_DIR> <DEST_DIR> <DAYS>[default 14 days] $N"
+    exit 1
+}
+
+if [ $# -le 2 ]; then
+   USAGE
+fi
