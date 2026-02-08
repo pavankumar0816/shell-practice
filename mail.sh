6e0512cd811830031c40055b94dc797c3bb587f3
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-02-08 20:30:36 +0530

    Updated

--- mail.sh
+++ mail.sh
@@ -7,12 +7,11 @@ SERVER_IP=$5
 TO_TEAM=$6
 FINAL_MESSAGE_BODY=$(echo $MESSAGE_BODY | sed -e 's/[]\/$*.^[]/\\&/g')
 
-FINAL_MESSAGE=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/SERVER_IP/$SERVER_IP/g" -e  "s/
-MESSAGE/$FINAL_MESSAGE_BODY/g" template.html)
+FINAL_MESSAGE=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/SERVER_IP/$SERVER_IP/g" -e  "s/MESSAGE/$FINAL_MESSAGE_BODY/g" template.html)
 {
 echo "To: $TO_ADDRESS"
 echo "Subject:$SUBJECT"
 echo "Content-Type: text/html"
 echo ""
 echo "$FINAL_MESSAGE"
-} | msmtp "TO_ADDRESS" 
+} | msmtp "$TO_ADDRESS" 
