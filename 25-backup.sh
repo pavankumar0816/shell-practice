2f1156194f3aa4783b671268d4e6c0e18025b7be
pavankumar0816 <manikantapavankumarpuppala@gmail.com>
2026-02-08 18:21:31 +0530

    Updated

--- 25-backup.sh
+++ 25-backup.sh
@@ -16,14 +16,14 @@ log(){
 }
 
 if [ $userid -ne 0 ]; then
-    -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    log "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
    exit 1
 fi
 
 mkdir -p $LOGS_FOLDER
 
 USAGE(){
-    log -e "$R USAGE:: sudo backup <source_DIR> <DEST_DIR> <DAYS>[default 14 days] $N"
+    log  "$R USAGE:: sudo backup <source_DIR> <DEST_DIR> <DAYS>[default 14 days] $N"
     exit 1
 }
 
@@ -34,12 +34,12 @@ if [ $# -lt 2 ]; then
 fi
 
 if [ ! -d $SOURCE_DIR ]; then
-   log -e "$Y Source Directory: $SOURCE_DIR Doest not exist $N"
+   log  "$Y Source Directory: $SOURCE_DIR Doest not exist $N"
    exit 1
 fi
 
 if [ ! -d $DEST_DIR ]; then
-   log -e "$Y Destination Directory: $DEST_DIR Does not exist $N"
+   log  "$Y Destination Directory: $DEST_DIR Does not exist $N"
    exit 1
 fi
 
